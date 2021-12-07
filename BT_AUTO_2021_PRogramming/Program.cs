8ef62914491f35bb409ef20eff98d664bbe86b02
DELL <DELL@DESKTOP-D5H6GHJ>
2021-12-07 23:01:59 +0200

    Add project files.

---
+++ BT_AUTO_2021_PRogramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT_AUTO_2021_PRogramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
