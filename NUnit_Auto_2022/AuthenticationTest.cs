1fdcb041e269224afdd9b29d033962d35a9f7515
DELL <DELL@DESKTOP-D5H6GHJ>
2022-01-17 10:35:33 +0200

    WebElements

---
+++ NUnit_Auto_2022/AuthenticationTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit_Auto_2022
+{
+    class AuthenticationTest
+    {
+        IWebDriver driver;
+        const string protocol = "https";
+        const string hostname = "86.121.249.150";
+        const string port = "4999";
+        const string path = "/#/login";
+
+        string url = protocol + "://" + hostname + ":" + port + path;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+        [TestCase("dinosaur", "dinosaur")]
+
+        [Test]
+        public void Test01()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            var username = driver.FindElement(By.Id("input-login-username"));
+            var password = driver.FindElement(By.Id("input-login-password"));
+            var submit = driver.FindElement(By.CssSelector("#login-form > div:nth-child(3) "));
+
+            username.SendKeys("beatrix");
+            password.SendKeys("1234QWER");
+
+            submit.Submit();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
--- NUnit_Auto_2022/SeleniumTests.cs
+++ NUnit_Auto_2022/SeleniumTests.cs
@@ -13,24 +13,37 @@ namespace NUnit_Auto_2022
     {
        // string path = "C:Desktop\\driver";
         IWebDriver driver;
+        const string protocol = "https";
+        const string hostname = "magazinulcolectionarului.ro/";
+        const string path = "/";
+
+        string url = protocol + "://" + hostname + path;
 
         [SetUp]
         public void Setup()
         {
-            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
             //driver = new FirefoxDriver();
-            driver = new EdgeDriver();
+           // driver = new EdgeDriver();
+            
         }
 
         [Test]
         public void Test01()
         {
-           
-           
-           driver.Url = "https://google.com";
+
+
+            //driver.Url = "https://google.com"; 
+           driver.Url = url; //am declarat mai sus calea
            driver.Navigate();
 
-            
+            // driver.Navigate().GoToUrl("https://google.com");
+
+            driver.Navigate().Back();
+
+            driver.Navigate().Forward();
+
+            driver.Navigate().Refresh();
         }
         [Test]
         public void Test02()
@@ -38,10 +51,39 @@ namespace NUnit_Auto_2022
             driver.Url = "https://learn.digitalstack.ro/login/index.php";
             driver.Navigate();
         }
+
+        [Test]
+        public void Test03()
+        {
+            driver.Url = url;
+            driver.Navigate();
+
+            IWebElement body = driver.FindElement(By.ClassName("modal-header"));
+            body.Click();
+
+            /*IWebElement cookieAccept1 = driver.FindElement(By.CssSelector(""));
+            cookieAccept1.Click();
+
+            IWebElement cookieAccept2 = driver.FindElement(By.XPath(""));
+            cookieAccept2.Click();*/
+
+            IWebElement eaglemoss = driver.FindElement(By.XPath("/html/body/div[3]/header/div[1]/ul/li/a/span"));
+            eaglemoss.Click();
+
+            var addToCart = driver.FindElements(By.XPath(""));
+            foreach(IWebElement el in addToCart)
+            {
+                var text = el.FindElement(By.XPath("//span"));
+                Console.WriteLine(text.Text);
+            }
+        }
         [TearDown]
         public void Teardown()
         {
             driver.Close();
         }
+
+
+
     }
 }
