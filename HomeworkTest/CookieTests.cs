e6da0aadab142ebdb5581e9e16758ac9e2360922
DELL <DELL@DESKTOP-D5H6GHJ>
2022-01-25 23:50:49 +0200

    Cookie Tests

---
+++ HomeworkTest/CookieTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkTest
+{
+    class CookieTests
+    {
+        IWebDriver driver;
+
+        const string protocol = "http";
+        const string hostname = "86.121.249.150";
+        const string port = "4999";
+        const string path = "/#/cookie";
+
+        string url = protocol + "://" + hostname + ":" + port + path;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestCookie()
+        {
+            driver.Navigate().GoToUrl(url);
+
+            var cookies = driver.Manage().Cookies;
+            Console.WriteLine("The site contains {0} cookies", cookies.AllCookies.Count);
+            foreach (Cookie c in cookies.AllCookies)
+            {
+                Console.WriteLine("I am getting the cookies from the browser:", c.Name, c.Value);
+            }
+
+            var setCookie = driver.FindElement(By.Id("set-cookie"));
+            var removeCookie = driver.FindElement(By.Id("delete-cookie"));
+            var cookieValue = driver.FindElement(By.Id("cookie-value"));
+
+            setCookie.Click();
+            removeCookie.Click();
+
+           
+
+            var ck = cookies.GetCookieNamed("PHPSESSID");
+            Console.WriteLine("Cookie name {0} and value {1}", cookies.AllCookies.Count);
+
+            cookies.DeleteAllCookies();
+            Console.WriteLine("The site contains {0} cookies", cookies.AllCookies.Count);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
