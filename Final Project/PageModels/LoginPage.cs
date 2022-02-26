e850fd3f802bccbce1852d0745640e79bcae4171
DELL <DELL@DESKTOP-D5H6GHJ>
2022-02-26 23:30:02 +0200

    Final project

---
+++ Final Project/PageModels/LoginPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Project.PageModels
+{
+    class LoginPage : BasePage
+    {
+        const string loginPageText = "login"; //id
+        const string emailLabel = "#customer_login > div:nth-child(4) > label"; //css
+        const string emailInput = "loginInputName"; //id
+        const string emailError = "#customer_login > div.tt-base-color > div > ul > li"; //css
+        const string passwordLabel = "#customer_login > div:nth-child(5) > label"; // css
+        const string passwordInput = "loginInputEmail"; // id
+        const string passwordError = "#customer_login > div.tt-base-color > div > ul > li"; //css
+        const string loginButton = "btn btn-border";//class
+        const string lostPassword = "#customer_login > div.row > div.col-auto.align-self-end > div > ul > li > a";
+
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+        public string CheckPage()
+        {
+            var loginPageEl = driver.FindElement(By.ClassName(loginPageText));
+            return loginPageEl.Text;
+        }
+        public void Login(string user, string passw)
+        {
+            var emailInputElement = driver.FindElement(By.Id(emailInput));
+            emailInputElement.Clear();
+            emailInputElement.SendKeys(user);
+            var passwordInputElement = driver.FindElement(By.Id(passwordInput));
+            passwordInputElement.Clear();
+            passwordInputElement.SendKeys(passw);
+            var loginButtonElement = driver.FindElement(By.ClassName(loginButton));
+            loginButtonElement.Submit();
+            var lostPasswordElement = driver.FindElement(By.CssSelector(lostPassword));
+            lostPasswordElement.Click();
+        }
+
+    }
+}
---
+++ Final Project/PageModels/RegisterPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Project.PageModels
+{
+    class RegisterPage
+    {
+        const string createAccountTextSelector = "#tt-pageContent > div > div > h1"; 
+        const string firstNameLabelSelector = "#create_customer > div:nth-child(4) > label"; 
+        const string firstNameInputSelector = "loginInputName";  // First Name is a * Required Field, but does not have an error message for empty values
+        const string lastNameLabelSelector = "#create_customer > div:nth-child(5) > label"; 
+        const string lastNameInputSelector = "loginLastName"; // Last Name is a * Required Field, but does not have an error message for empty values
+        const string emailLabelSelector = "#create_customer > div:nth-child(6) > label";
+        const string emailInputSelector = "loginInputEmail";
+        const string emailErrorSelector = "#create_customer > div.tt-base-color > div > ul > li:nth-child(2)";
+        const string passwordLabelSelector = "#create_customer > div:nth-child(7) > label";
+        const string passwordInputSelector = "loginInputPassword";
+        const string passwordErrorSelector = "#create_customer > div.tt-base-color > div > ul > li:nth-child(1)";
+        const string createSelector = "#create_customer > div.row > div:nth-child(1) > div > button"; 
+        const string returnToStoreSelector = "#create_customer > div.row > div.col-auto.align-self-center > div > ul > li > a"; 
+    }
+}
